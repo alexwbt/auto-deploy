4c8b55cdf95b7dbf35457dc7304263207858ed6d
alexwbt <alexwbtg@gmail.com>
2023-02-01 16:02:02 +0800

    update syncPackage

--- src/functions/syncPackage.ts
+++ src/functions/syncPackage.ts
@@ -1,6 +1,8 @@
+import fs from "fs";
 import Remote from "../remote/Remote";
 import { RemoteExecResult } from "../remote/RemoteClient";
 import buildEnvFile from "./buildEnvFile";
+import renderDirectory from "./renderDirectory";
 
 export type SyncConfig = {
   /**
@@ -9,6 +11,11 @@ export type SyncConfig = {
    * passing empty string will result to root directory "/"
    */
   dir?: string;
+
+  /**
+   * Package directory
+   */
+  package?: string;
 
   /**
    * List of name to keep while sync package
@@ -22,16 +29,21 @@ export type SyncConfig = {
 };
 
 const syncPackage = async (remote: Remote, domain: string, config: SyncConfig = {}) => {
-  const package_ = "package";
-  const packageDomainDir = `${package_}/${domain}`;
+  const packageTmpDir = "package_tmp";
+  const packageDomainDir = `${packageTmpDir}/${domain}`;
   const destDomainDir = `${config.dir || "~"}/${domain}`;
 
-  // build env
-  const env = await buildEnvFile(`${packageDomainDir}/.env`, {
+  const envData = {
     ...config.env,
     "WORK_DIR": destDomainDir,
-  });
-  console.log(`-------\n.env\n-------\n${env}\n-------`);
+  };
+
+  // render template
+  await renderDirectory(config.package || "package", packageTmpDir, envData);
+
+  // build env
+  const envResult = await buildEnvFile(`${packageDomainDir}/.env`, envData);
+  console.log(`-------\n.env\n-------\n${envResult}\n-------`);
 
   // pre-upload checks
   const testRes = await remote.client
@@ -61,7 +73,7 @@ const syncPackage = async (remote: Remote, domain: string, config: SyncConfig = 
   await remote.client.exec(`cp -rfu -t ${destDomainDir} `
     + `${destDomainDir}/${packageDomainDir}/* `
     + `${destDomainDir}/${packageDomainDir}/.[^.]*`);
-  await remote.client.exec(`rm -rf ${destDomainDir}/${package_}`);
+  await remote.client.exec(`rm -rf ${destDomainDir}/${packageTmpDir}`);
 
   // commit
   if (init) {
@@ -83,6 +95,9 @@ const syncPackage = async (remote: Remote, domain: string, config: SyncConfig = 
   }).catch(() => {
     console.warn("Failed To Commit");
   });
+
+  // remove packageTmp
+  await fs.promises.rm(packageTmpDir, { recursive: true, force: true });
 
   console.log("Sync Package Complete");
 };
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,18 @@
-// // import initialize from "./functions/initialize";
-// import syncPackage from "./functions/syncPackage";
-// import remoteSession from "./remote/remoteSession";
-// import { getEnvString, getEnvStringRequired } from "./utils/env";
+// import initialize from "./functions/initialize";
+import syncPackage from "./functions/syncPackage";
+import remoteSession from "./remote/remoteSession";
+import { getEnvStringRequired } from "./utils/env";
 
-import renderDirectory from "./functions/renderDirectory";
+remoteSession(async remote => {
+  // await initialize(remote);
 
-// remoteSession(async remote => {
-//   // await initialize(remote);
-
-//   await syncPackage(remote, "alexwbt.com", {
-//     dir: "~/prod",
-//     keep: ["runtime"],
-//     env: {
-//       "USER": getEnvString("REMOTE_USER", "ec2-user"),
-//       "CERT_EMAIL": getEnvStringRequired("CERT_EMAIL"),
-//     },
-//   });
-// });
-
-renderDirectory("package", "package_tmp", {
-  // ALEXWBT_DOMAIN: "alexwbt.com"
+  await syncPackage(remote, "alexwbt.com", {
+    dir: "~/prod",
+    keep: ["runtime"],
+    env: {
+      "USER": getEnvStringRequired("REMOTE_USER"),
+      "CERT_EMAIL": getEnvStringRequired("CERT_EMAIL"),
+      "ALEXWBT_DOMAIN": getEnvStringRequired("ALEXWBT_DOMAIN"),
+    },
+  });
 });
